3110517743d32e03e34929efc48422d28b81c3a5
Tyler <Tconfalone@gmail.com>
2018-03-24 10:06:47 -0700

    start of insertDashes

---
+++ insertDashes/insertDashes.js
@@ -0,0 +1,8 @@
+function insertDash(num) {
+    //code me
+   let nums = num.toString().split('');
+   console.log(nums);
+   for (let i=0; i < nums.length; i++) {
+     if (i === '1 || 3 || 5 || 7 || 9' && i++ === '1 || 3 || 5 || 7 || 9') {
+       
+   }
