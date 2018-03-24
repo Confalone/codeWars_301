5d8b842696aa71c9e4d366bbef5e8fe950ab73c1
Tyler <Tconfalone@gmail.com>
2018-03-24 10:27:42 -0700

    trying

--- insertDashes/insertDashes.js
+++ insertDashes/insertDashes.js
@@ -1,8 +1,14 @@
+
+let numbers = 21312443424;
 function insertDash(num) {
-    //code me
-   let nums = num.toString().split('');
-   console.log(nums);
+  //code me
+  let nums = num.toString().split('');
+  console.log(nums);
+  console.log();
    for (let i=0; i < nums.length; i++) {
-     if (i === '1 || 3 || 5 || 7 || 9' && i++ === '1 || 3 || 5 || 7 || 9') {
-       
-   }
+       console.log(nums);
+//      if ({
+
+//    }
+}
+insertDash(numbers);
