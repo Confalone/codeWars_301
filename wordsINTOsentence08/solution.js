26dac7b2d8574c21917fa3a0240cdb1ae045b1d7
Tyler <Tconfalone@gmail.com>
2018-04-09 21:58:49 -0700

    solutioned

--- wordsINTOsentence08/solution.js
+++ wordsINTOsentence08/solution.js
@@ -1,8 +1,24 @@
 function formatWords(words){
-    //   if (words === '' || words === null return('');
-    for (let i=0; i>words.length; i++){
-    if (words[i] === ('')) {words[i].splice([i],1)}
-    let sl = words[i]-2;
-    let last = words[i]-1;
+  let refinedWords = [];
+  let sentence = [];
+  if (words === null || words.length === 0 || (words.length === 1 && words[0] === '')) {return'';}
+  for (let i = 0; i < words.length; i++) {
+    if (words[i]) {
+      refinedWords.push(words[i]);
     }
-    return(words+sl+'and' +last);
+  }
+  for (let j = 0; j < refinedWords.length; j++) {
+    console.log(refinedWords);
+    if (refinedWords.length === 1) {
+      return refinedWords[j];
+    } else if (refinedWords.length === 2) {
+      sentence.push(refinedWords[j] + ' and ' + refinedWords[j+1]);
+      return sentence.join('');
+    } else if (j+2 !== refinedWords.length) {
+      sentence.push(refinedWords[j] + ', ');
+    } else {
+      sentence.push(refinedWords[j] + ' and ' + refinedWords[j+1]);
+      return sentence.join('');
+    }
+  }
+}
