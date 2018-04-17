aabde7ce1409326422ce9d662e13a379f51d0f40
Tyler Confalone <tconfalone@gmail.com>
2018-04-16 21:00:08 -0700

    lab 10

--- useMap/solution.js
+++ useMap/solution.js
@@ -1,10 +1,4 @@
 function double(array) {
-    let newArr = [];
-    let newNum = 0;
-    for(let i = 0; i < array.length; i++){
-      newNum = 0;
-      newNum = array[i] * 2;
-      newArr.push(newNum);
-    }
-    return newArr;
+  const map1 = array.map(x => x*2);
+ return map1;
  }
